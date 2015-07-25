cb73d11bfe96d935f66bb356e602e1e9b03571fb
Oleg Bogorodskiy <oleg.bogorodskiy@gmail.com>
2015-07-25 19:26:19 +0300

    assignment 2 almost done

--- assignment_2/script.js
+++ assignment_2/script.js
@@ -62,17 +62,44 @@ function addTriangles()
 	
 	var v1 = vec2(-vector[1] / vectorLength, vector[0] / vectorLength);
 	toBrushSizeVector(v1);	
-	
 	var v2 = vec2(-v1[0], -v1[1]);
+	var v3 = vec2(-v1[0] + vector[0], -v1[1] + vector[1]);
+	var v4 = vec2(v1[0] + vector[0], v1[1] + vector[1]);
 	
-	var v3 = vec2(v1[0] + vector[0], v1[1] + vector[1]);
+	// connect start points with end points in the middle (intersection of 2 lines)
+	if (vertices.length != 0)
+	{
+		var line1Start = v4;
+		var line1End = v1;
+		var line2Start = vec2(vertices[vertices.length - 2][0] - point1[0], 
+							  vertices[vertices.length - 2][1] - point1[1]); // old v1
+		var line2End = vec2(vertices[vertices.length - 1][0] - point1[0], 
+							vertices[vertices.length - 1][1] - point1[1]); // old v4
+		var middlePoint1 = getIntersection(line1Start, line1End, line2Start, line2End);
+		v1 = middlePoint1;
 
-	var v4 = vec2(-v1[0] + vector[0], -v1[1] + vector[1]);
-
+		line1Start = v3;
+		line1End = v2;
+		line2Start = vec2(vertices[vertices.length - 5][0] - point1[0], 
+						  vertices[vertices.length - 5][1] - point1[1]); // old v2
+		line2End = vec2(vertices[vertices.length - 3][0] - point1[0], 
+						vertices[vertices.length - 3][1] - point1[1]); // old v3
+		var middlePoint2 = getIntersection(line1Start, line1End, line2Start, line2End);
+		v2 = middlePoint2;
+		
+		vertices[vertices.length - 1] = vec2(middlePoint1[0] + point1[0], middlePoint1[1] + point1[1]);
+		vertices[vertices.length - 3] = vec2(middlePoint2[0] + point1[0], middlePoint2[1] + point1[1]);
+		vertices[vertices.length - 4] = vec2(middlePoint2[0] + point1[0], middlePoint2[1] + point1[1]);
+	}
+	
 	vertices[vertices.length] = vec2(point1[0] + v1[0], point1[1] + v1[1]);
 	vertices[vertices.length] = vec2(point1[0] + v2[0], point1[1] + v2[1]);
 	vertices[vertices.length] = vec2(point1[0] + v3[0], point1[1] + v3[1]);
+	
+	vertices[vertices.length] = vec2(point1[0] + v3[0], point1[1] + v3[1]);
+	vertices[vertices.length] = vec2(point1[0] + v1[0], point1[1] + v1[1]);
 	vertices[vertices.length] = vec2(point1[0] + v4[0], point1[1] + v4[1]);
+	
 	
 	gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
 	gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
@@ -80,6 +107,29 @@ function addTriangles()
 	console.log(vertices.length);
 	
 	render();
+}
+
+function getIntersection(line1Start, line1End, line2Start, line2End)
+{
+	var intersectionX;
+	var intersectionY;
+	
+	var dx = line1End[0] - line1Start[0];
+	var dy = line1End[1] - line1Start[1];
+	
+	var m1 = dy / dx;
+	var c1 = line1Start[1] - m1 * line1Start[0];
+	
+	dx = line2End[0] - line2Start[0];
+	dy = line2End[1] - line2Start[1];
+	
+	var m2 = dy / dx;
+	var c2 = line2End[1] - m2 * line2End[0];
+	
+	intersectionX = (c2 - c1) / (m1 - m2);
+	intersectionY = m1 * intersectionX + c1;
+	
+	return vec2(intersectionX, intersectionY);
 }
 
 function toBrushSizeVector(unitVector)
