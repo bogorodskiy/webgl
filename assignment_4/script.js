18fe626a09ca750b01a64fc075e97a8add30481e
Oleg Bogorodskiy <oleg.bogorodskiy@gmail.com>
2015-08-24 22:37:04 +0300

    assignment 4 sceleton

---
+++ assignment_4/script.js
@@ -0,0 +1,447 @@
+"use strict";
+
+var OPERATION_TRANSLATION = 0;
+var OPERATION_ROTATION = 1;
+var OPERATION_SCALE = 2;
+
+var canvas = null;
+var canvasBounds = null;
+var gl;
+var shapes = [];
+var program = null;
+var currentOperation = OPERATION_TRANSLATION;
+var currentShapeType = null;
+var selectedShape = null;
+var createFunctionByType = {};
+var shapeById = {};
+var fovy = 45;
+var aspect = 1.0;
+var near = 0.1;
+var far = 10.0;
+
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+
+window.onload = function init() 
+{
+	createFunctionByType[SHAPE_SPHERE] = createSphere;
+	createFunctionByType[SHAPE_CONE] = createCone;
+	createFunctionByType[SHAPE_CYLINDER] = createCylinder;
+	
+	canvas = document.getElementById("gl-canvas");
+	canvasBounds = canvas.getBoundingClientRect();
+	$("#addButton").on("click", onAddButtonClick);
+	$("#deleteButton").on("click", onDeleteButtonClick);
+	$("#colorPicker").on("change", onColorChange);
+	
+	$("#xSlider").on("slide", onXChange);
+	$("#xSlider").on("slidechange", onXChange);
+	onXChange();
+	$("#ySlider").on("slide", onYChange);
+	$("#ySlider").on("slidechange", onYChange);
+	onYChange();
+	$("#zSlider").on("slide", onZChange);
+	$("#zSlider").on("slidechange", onZChange);
+	onZChange();
+	
+	$("#translationSelector").click(onOperationTypeChange);
+	$("#rotationSelector").click(onOperationTypeChange);
+	$("#scaleSelector").click(onOperationTypeChange);
+	onOperationTypeChange();
+
+	$("#sphereSelector").click(onShapeTypeChange);
+	$("#coneSelector").click(onShapeTypeChange);
+	$("#cylinderSelector").click(onShapeTypeChange);
+	onShapeTypeChange();
+	
+	$("#selectedShapeList").selectmenu({ width : 'auto'});
+	$("#selectedShapeList").on("selectmenuselect", onShapeSelected);
+
+	gl = WebGLUtils.setupWebGL(canvas);
+	if (!gl) 
+	{ 
+		console.log("WebGL isn't available"); 
+	}
+
+	aspect = canvas.width / canvas.height;
+	gl.viewport(0, 0, canvas.width, canvas.height);
+	gl.clearColor(0.0, 0.0, 0.0, 1);
+	gl.enable(gl.DEPTH_TEST);
+	//gl.enable(gl.CULL_FACE);
+	//
+	//  Load shaders and initialize attribute buffers
+	//
+	program = initShaders(gl, "vertex-shader", "fragment-shader");
+	gl.useProgram(program);
+	
+	program.vPosition = gl.getAttribLocation(program, "vPosition");
+	gl.enableVertexAttribArray(program.vPosition);
+	
+	program.vColor = gl.getAttribLocation(program, "vColor");
+	gl.enableVertexAttribArray(program.vColor);
+	
+    program.vNormal = gl.getAttribLocation(program, "vNormal");
+    gl.enableVertexAttribArray(program.vNormal);
+		
+	program.modelViewMatrix = gl.getUniformLocation(program, "modelViewMatrix");
+	program.projectionMatrix = gl.getUniformLocation(program, "projectionMatrix");
+	
+	render();
+}
+
+function getColor()
+{
+	var colorStr = $("#colorPicker").val().toString();
+	var red = parseInt(colorStr.substr(0, 2), 16);
+	var green = parseInt(colorStr.substr(2, 2), 16);
+	var blue = parseInt(colorStr.substr(4, 2), 16);
+	var result = vec4(red / 255, green / 255, blue / 255);
+	return result;
+}
+
+function updateSliders()
+{
+	var xValue = 0.0;
+	var yValue = 0.0;
+	var zValue = 0.0;
+	
+	var xmin = 0.0;
+	var xmax = 0.5;
+	var xstep = 0.05;
+
+	var ymin = 0.0;
+	var ymax = 0.5;
+	var ystep = 0.05;
+
+	var zmin = 0.0;
+	var zmax = 1.0;
+	var zstep = 0.1;
+	
+	if (selectedShape != null)
+	{
+		if (currentOperation == OPERATION_TRANSLATION)
+		{
+			xValue = selectedShape[SHAPE_TRANSLATION_X];
+			yValue = selectedShape[SHAPE_TRANSLATION_Y];
+			zValue = selectedShape[SHAPE_TRANSLATION_Z];
+			
+			xmin = -0.5;
+			ymin = -0.5;
+			zmin = -4;
+			zmax = -0.2;
+			zstep = 0.1;
+		}
+		else if (currentOperation == OPERATION_ROTATION)
+		{
+			xValue = selectedShape[SHAPE_ROTATION_X] * 180 / Math.PI;
+			yValue = selectedShape[SHAPE_ROTATION_Y] * 180 / Math.PI;
+			zValue = selectedShape[SHAPE_ROTATION_Z] * 180 / Math.PI;
+			
+			xmin = -180.0;
+			xmax = 180.0;
+			xstep = 1.0;
+			
+			ymin = -180.0;
+			ymax = 180.0;
+			ystep = 1.0;
+			
+			zmin = -180.0;
+			zmax = 180.0;
+			zstep = 1.0;
+		}
+		else if (currentOperation == OPERATION_SCALE)
+		{
+			xValue = selectedShape[SHAPE_SCALE_X];
+			yValue = selectedShape[SHAPE_SCALE_Y];
+			zValue = selectedShape[SHAPE_SCALE_Z];
+			
+			xmin = 0.1;
+			ymin = 0.1;
+			zmin = 0.1;
+			xmax = 3.0;
+			ymax = 3.0;
+			zmax = 3.0;
+		}
+	}
+	
+	$("#xSlider").slider("option", "min", xmin);
+	$("#xSlider").slider("option", "max", xmax);
+	$("#xSlider").slider("option", 'step', xstep);
+	
+	$("#ySlider").slider("option", "min", ymin);
+	$("#ySlider").slider("option", "max", ymax);
+	$("#ySlider").slider("option", 'step', ystep);
+	
+	$("#zSlider").slider("option", "min", zmin);
+	$("#zSlider").slider("option", "max", zmax);
+	$("#zSlider").slider("option", 'step', zstep);
+
+	$("#xSlider").slider('value', xValue);
+	$("#ySlider").slider('value', yValue);
+	$("#zSlider").slider('value', zValue);
+	onXChange();
+	onYChange();
+	onZChange();
+}
+
+function updateLight()
+{
+	var ambientProduct = mult(lightAmbient, selectedShape[MATERIAL_AMBIENT]);
+	var diffuseProduct = mult(lightDiffuse, selectedShape[MATERIAL_DIFFUSE]);
+    var specularProduct = mult(lightSpecular, selectedShape[MATERIAL_SPECULAR]);
+	
+    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct) );
+    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );	
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition) );
+    gl.uniform1f(gl.getUniformLocation(program, "shininess"), selectedShape[MATERIAL_SHININESS]);
+}
+
+function updateShapeProperties()
+{
+	if (selectedShape == null)
+	{
+		return;
+	}
+	
+	if (currentOperation == OPERATION_TRANSLATION)
+	{
+		selectedShape.setTranslation( Number( $("#xSlider").slider("option", "value") ),
+							  Number( $("#ySlider").slider("option", "value") ),
+							  Number( $("#zSlider").slider("option", "value") ));
+	}
+	else if (currentOperation == OPERATION_ROTATION)
+	{
+		var rotationX = Number( $("#xSlider").slider("option", "value") );
+		var rotationY = Number( $("#ySlider").slider("option", "value") );
+		var rotationZ = Number( $("#zSlider").slider("option", "value") );
+
+		selectedShape.setRotationX( rotationX * Math.PI / 180 );
+		selectedShape.setRotationY( rotationY * Math.PI / 180 );
+		selectedShape.setRotationZ( rotationZ * Math.PI / 180 );
+	}
+	else if (currentOperation == OPERATION_SCALE)
+	{
+		selectedShape.setScale( Number( $("#xSlider").slider("option", "value") ),
+							  Number( $("#ySlider").slider("option", "value") ),
+							  Number( $("#zSlider").slider("option", "value") ) );
+	}
+	
+	render();
+}
+
+function degreeToRadians(value)
+{
+	return value * Math.PI / 180.0;
+}
+
+function onAddButtonClick(event)
+{
+	var createFunction = createFunctionByType[currentShapeType];
+	if (createFunction == null)
+	{
+		console.log("No function for shape type", currentShapeType);
+		return;
+	}
+	
+	var shape = createFunction(getColor());
+	
+	var normalBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape[SHAPE_NORMALS]), gl.STATIC_DRAW);
+	normalBuffer.normalSize = 3;
+	normalBuffer.numItems = shape[SHAPE_NORMALS] / 3;
+	
+	var vertexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape[SHAPE_VERTICES]), gl.STATIC_DRAW);
+	vertexBuffer.vertexSize = 3;
+	vertexBuffer.numItems = shape[SHAPE_VERTICES].length / 3;
+	
+	var indexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(shape[SHAPE_INDICES]), gl.STATIC_DRAW);
+	indexBuffer.indexSize = 1;
+	indexBuffer.numItems = shape[SHAPE_INDICES].length;
+
+	var colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape[SHAPE_COLORS]), gl.STATIC_DRAW);
+	colorBuffer.colorSize = 4;
+	colorBuffer.numItems = shape[SHAPE_COLORS].length;
+	
+	shape[SHAPE_VERTEX_BUFFER] = vertexBuffer;
+	shape[SHAPE_INDEX_BUFFER] = indexBuffer;
+	shape[SHAPE_COLOR_BUFFER] = colorBuffer;
+	shape[SHAPE_NORMAL_BUFFER] = normalBuffer;
+	
+	shapes[shapes.length] = shape;
+	shapeById[shape[SHAPE_ID]] = shape;
+	selectedShape = shape;
+
+	$('#selectedShapeList').append("<option value='" + selectedShape[SHAPE_ID] + "'>" + selectedShape[SHAPE_ID] + "</option");
+	$('#selectedShapeList').val(selectedShape[SHAPE_ID]);
+	$("#selectedShapeList").selectmenu("refresh");
+	
+	updateSliders();
+	render();
+}
+
+function onColorChange(event)
+{
+	if (selectedShape)
+	{
+		selectedShape.setColor(getColor());
+		var colorBuffer = selectedShape[SHAPE_COLOR_BUFFER];
+		gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(selectedShape[SHAPE_COLORS]), gl.STATIC_DRAW);
+		render();
+	}
+}
+
+function onOperationTypeChange(event)
+{
+	var checkedId = $("#operationSelector :radio:checked").attr('id');
+	
+	if (checkedId == "translationSelector")
+	{
+		currentOperation = OPERATION_TRANSLATION;
+	}
+	else if(checkedId == "rotationSelector")
+	{
+		currentOperation = OPERATION_ROTATION;
+	}
+	else if(checkedId == "scaleSelector")
+	{
+		currentOperation = OPERATION_SCALE;
+	}
+	
+	updateSliders();
+}
+
+function onShapeTypeChange(event)
+{
+	var checkedId = $("#shapeSelector :radio:checked").attr('id');
+	
+	if (checkedId == "sphereSelector")
+	{
+		currentShapeType = SHAPE_SPHERE;
+	}
+	else if(checkedId == "coneSelector")
+	{
+		currentShapeType = SHAPE_CONE;
+	}
+	else if(checkedId == "cylinderSelector")
+	{
+		currentShapeType = SHAPE_CYLINDER;
+	}
+}
+
+function onXChange(event, ui)
+{
+	var userInteraction = (event != null && event.view != null);
+
+	var x = ui ? ui.value : $("#xSlider").slider("option", "value");
+	$("#xCaption").text("X = " + x);
+	if (userInteraction)
+	{
+		updateShapeProperties();
+	}
+}
+function onYChange(event, ui)
+{
+	var userInteraction = (event != null && event.view != null);
+	
+	var y =  ui ? ui.value : $("#ySlider").slider("option", "value");
+	$("#yCaption").text("Y = " + y);
+	if (userInteraction)
+	{
+		updateShapeProperties();
+	}
+}
+function onZChange(event, ui)
+{
+	var userInteraction = (event != null && event.view != null);
+	
+	var z = ui ? ui.value : $("#zSlider").slider("option", "value");
+	$("#zCaption").text("Z = " + z);
+	if (userInteraction)
+	{
+		updateShapeProperties();
+	}
+}
+
+function onShapeSelected(event, ui)
+{
+	var shapeId = $('#selectedShapeList').val();
+	selectedShape = shapeById[shapeId];
+	updateSliders();
+}
+
+function onDeleteButtonClick(event)
+{
+	if (selectedShape != null)
+	{
+		var shapeId = selectedShape[SHAPE_ID];
+		delete shapeById[shapeId];
+		var n = shapes.length;
+		for (var i = 0; i < n; i++)
+		{
+			if (shapes[i][SHAPE_ID] == shapeId)
+			{
+				shapes.splice(i, 1);
+				break;
+			}
+		}
+		
+		$('#selectedShapeList :selected').remove();
+		$("#selectedShapeList").selectmenu("refresh");
+		
+		selectedShape = null;
+		updateSliders();
+		render();
+	}
+}
+
+function render() 
+{
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	
+	var n = shapes.length;
+	for (var i = 0; i < n; i++)
+	{
+		var shape = shapes[i];
+		
+		var vertexBuffer = shape[SHAPE_VERTEX_BUFFER];
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.vertexAttribPointer(program.vPosition, vertexBuffer.vertexSize, gl.FLOAT, false, 0, 0);
+
+		var normalBuffer = shape[SHAPE_NORMAL_BUFFER];
+        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+        gl.vertexAttribPointer(program.vNormal, normalBuffer.normalSize, gl.FLOAT, false, 0, 0);
+		
+		var indexBuffer = shape[SHAPE_INDEX_BUFFER];
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+		
+		var colorBuffer = shape[SHAPE_COLOR_BUFFER];
+		gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+		gl.vertexAttribPointer(program.vColor, colorBuffer.colorSize, gl.FLOAT, false, 0, 0);
+		
+		var modelViewMatrix = mat4();
+
+		modelViewMatrix = mult(modelViewMatrix, shape.translation);
+		modelViewMatrix = mult(modelViewMatrix, shape.rotationX);
+		modelViewMatrix = mult(modelViewMatrix, shape.rotationY);
+		modelViewMatrix = mult(modelViewMatrix, shape.rotationZ);
+		modelViewMatrix = mult(modelViewMatrix, shape.scale);
+
+		gl.uniformMatrix4fv(program.modelViewMatrix, false, flatten(modelViewMatrix));
+		
+		var projectionMatrix = perspective(fovy, aspect, near, far);
+		gl.uniformMatrix4fv(program.projectionMatrix, false, flatten(projectionMatrix));
+		
+        gl.drawElements(gl.TRIANGLES, indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+	}
+}
---
+++ assignment_4/shape.js
@@ -0,0 +1,380 @@
+// thanks to http://learningwebgl.com/blog/
+
+var SHAPE_SPHERE = "sphere";
+var SHAPE_CONE = "cone";
+var SHAPE_CYLINDER = "cylinder";
+
+var SHAPE_ID = "shape_id";
+var SHAPE_TYPE = "type";
+var SHAPE_VERTICES = "vertices";
+var SHAPE_INDICES = "indices";
+var SHAPE_NORMALS = "normals";
+var SHAPE_TEXTURE_COORDINATES = "texture_coordinates";
+var SHAPE_COLORS = "colors";
+
+var SHAPE_VERTEX_BUFFER = "vertex_buffer";
+var SHAPE_COLOR_BUFFER = "color_buffer";
+var SHAPE_NORMAL_BUFFER = "normal_buffer";
+var SHAPE_TEXTURE_COORD_BUFFER = "v_buffer";
+var SHAPE_INDEX_BUFFER = "index_buffer";
+
+var SHAPE_TRANSLATION_X = "shape_translation_x";
+var SHAPE_TRANSLATION_Y = "shape_translation_y";
+var SHAPE_TRANSLATION_Z = "shape_translation_z";
+
+var SHAPE_ROTATION_X = "shape_rotation_x";
+var SHAPE_ROTATION_Y = "shape_rotation_y";
+var SHAPE_ROTATION_Z = "shape_rotation_z";
+
+var SHAPE_SCALE_X = "shape_scale_x";
+var SHAPE_SCALE_Y = "shape_scale_y";
+var SHAPE_SCALE_Z = "shape_scale_z";
+
+var MATERIAL_AMBIENT = "material_ambient";
+var MATERIAL_DIFFUSE = "material_diffuse";
+var MATERIAL_SPECULAR = "material_specular";
+var MATERIAL_SHININESS = "material_shininess";
+
+var spheresCount = 0;
+var conesCount = 0;
+var cylindersCount = 0;
+
+function createSphere(color)
+{
+	var radius = 0.1;
+	var vertices = [];
+	var normals = [];
+	var textureCoordinates = [];
+	var indices = [];
+	var latitudeBands = 30;
+	var longitudeBands = 30;
+	
+	for (var latNumber = 0; latNumber <= latitudeBands; latNumber++)
+	{
+		var theta = latNumber * Math.PI / latitudeBands;
+		var sinTheta = Math.sin(theta);
+		var cosTheta = Math.cos(theta);
+		
+		for (var longNumber = 0; longNumber <= longitudeBands; longNumber++)
+		{
+			var phi = longNumber * 2 * Math.PI / longitudeBands;
+			var sinPhi = Math.sin(phi);
+			var cosPhi = Math.cos(phi);
+			
+			var x = cosPhi * sinTheta;
+			var y = cosTheta;
+			var z = sinPhi * sinTheta;
+			var u = 1 - (longNumber / longitudeBands);
+			var v = 1 - (latNumber / latitudeBands);
+			
+			normals[normals.length] = x;
+			normals[normals.length] = y;
+			normals[normals.length] = z;
+			
+			//textureCoordinates[textureCoordinates.length] = u;
+			//textureCoordinates[textureCoordinates.length] = v;
+			addVertex(vertices, x * radius, y * radius, z * radius);
+		}
+	}
+	
+	for (latNumber = 0; latNumber < latitudeBands; latNumber++)
+	{
+		for (longNumber = 0; longNumber < longitudeBands; longNumber++)
+		{
+			var first = (latNumber * (longitudeBands + 1)) + longNumber;
+			var second = first + longitudeBands + 1;
+			indices[indices.length] = first;
+			indices[indices.length] = second;
+			indices[indices.length] = first + 1;
+			
+			indices[indices.length] = second;
+			indices[indices.length] = second + 1;
+			indices[indices.length] = first + 1;
+		}
+	}
+	
+	spheresCount++;
+	
+	var result = createShape();
+	result[SHAPE_ID] = SHAPE_SPHERE + "_" + spheresCount.toString();
+	result[SHAPE_TYPE] = SHAPE_SPHERE;
+	result[SHAPE_VERTICES] = vertices;
+	result[SHAPE_INDICES] = indices;
+	result[SHAPE_NORMALS] = normals;
+	result[SHAPE_TEXTURE_COORDINATES] = textureCoordinates;
+	result.setColor(color);
+
+	return result;
+}
+
+function createCone(color)
+{
+	var radius = 0.1;
+	var height = 0.2;
+	var vertices = [];
+	var normals = [];
+	var textureCoordinates = [];
+	var indices = [];
+	var edgesCount = 30;
+	var yOffset = -0.1;
+
+	// bottom center
+	addVertex(vertices, 0, yOffset, 0);
+	
+	// bottom circle
+	for (var i = 0; i < edgesCount; i++)
+	{
+		var phi = i * 2 * Math.PI / edgesCount;
+		var sinPhi = Math.sin(phi);
+		var cosPhi = Math.cos(phi);
+		addVertex(vertices, cosPhi * radius, yOffset, sinPhi * radius);
+	}
+
+	// top point
+	addVertex(vertices, 0, height + yOffset, 0);
+	
+	var topVertexIndex = (vertices.length / 3) - 1;
+
+	for (i = 2; i <= edgesCount; i++)
+	{
+		indices[indices.length] = i;
+		indices[indices.length] = i - 1;
+		indices[indices.length] = 0;
+		
+		indices[indices.length] = i - 1;
+		indices[indices.length] = i;
+		indices[indices.length] = topVertexIndex;
+	}
+	indices[indices.length] = 1;
+	indices[indices.length] = edgesCount;
+	indices[indices.length] = 0;
+	
+	indices[indices.length] = edgesCount;
+	indices[indices.length] = 1;
+	indices[indices.length] = topVertexIndex;
+	
+	conesCount++;
+	
+	var result = createShape();
+	result[SHAPE_ID] = SHAPE_CONE + "_" + conesCount.toString();
+	result[SHAPE_TYPE] = SHAPE_CONE;
+	result[SHAPE_VERTICES] = vertices;
+	result[SHAPE_INDICES] = indices;
+	result[SHAPE_NORMALS] = normals;
+	result[SHAPE_TEXTURE_COORDINATES] = textureCoordinates;
+	result.setColor(color);
+
+	return result;
+}
+
+function createCylinder(color)
+{
+	var radius = 0.1;
+	var height = 0.2;
+	var vertices = [];
+	var normals = [];
+	var textureCoordinates = [];
+	var indices = [];
+	var edgesCount = 30;
+	var yOffset = -0.1;
+
+	// bottom center
+	addVertex(vertices, 0, yOffset, 0);
+	
+	for (var i = 0; i < edgesCount; i++)
+	{
+		var phi = i * 2 * Math.PI / edgesCount;
+		var sinPhi = Math.sin(phi);
+		var cosPhi = Math.cos(phi);
+		
+		var x = cosPhi * radius;
+		var y = yOffset;
+		var z = sinPhi * radius;
+		
+		addVertex(vertices, x, y, z);
+		addVertex(vertices, x, y + height, z);
+	}
+
+	// top center
+	addVertex(vertices, 0, height + yOffset, 0);
+	
+	var topVertexIndex = (vertices.length / 3) - 1;
+
+	for (i = 3; i <= edgesCount * 2; i++)
+	{
+		if (i % 2 != 0)
+		{
+			indices[indices.length] = i;
+			indices[indices.length] = i - 2;
+			indices[indices.length] = 0;
+		}
+		else
+		{
+			indices[indices.length] = i - 2;
+			indices[indices.length] = i - 3;
+			indices[indices.length] = i - 1;
+
+			indices[indices.length] = i - 2;
+			indices[indices.length] = i - 1;
+			indices[indices.length] = i;
+
+			indices[indices.length] = topVertexIndex;
+			indices[indices.length] = i - 2;
+			indices[indices.length] = i;
+		}
+	}
+
+	indices[indices.length] = i - 1;
+	indices[indices.length] = 1;
+	indices[indices.length] = 2;
+	
+	indices[indices.length] = 1;
+	indices[indices.length] = i - 1;
+	indices[indices.length] = i - 2;
+	
+	indices[indices.length] = 1;
+	indices[indices.length] = edgesCount * 2 - 1;
+	indices[indices.length] = 0;
+	
+	indices[indices.length] = edgesCount * 2;
+	indices[indices.length] = 2;
+	indices[indices.length] = topVertexIndex;
+	
+	conesCount++;
+	
+	var result = createShape();
+	result[SHAPE_ID] = SHAPE_CYLINDER + "_" + cylindersCount.toString();
+	result[SHAPE_TYPE] = SHAPE_CYLINDER;
+	result[SHAPE_VERTICES] = vertices;
+	result[SHAPE_INDICES] = indices;
+	result[SHAPE_NORMALS] = normals;
+	result[SHAPE_TEXTURE_COORDINATES] = textureCoordinates;
+	result.setColor(color);
+
+	return result;
+}
+
+function createShape()
+{
+	var result = {};
+	
+	result[SHAPE_TRANSLATION_X] = 0.0;
+	result[SHAPE_TRANSLATION_Y] = 0.0;
+	result[SHAPE_TRANSLATION_Z] = 0.0;
+
+	result[SHAPE_ROTATION_X] = 0.0;
+	result[SHAPE_ROTATION_Y] = 0.0;
+	result[SHAPE_ROTATION_Z] = 0.0;
+
+	result[SHAPE_SCALE_X] = 1.0;
+	result[SHAPE_SCALE_Y] = 1.0;
+	result[SHAPE_SCALE_Z] = 1.0;
+	
+	result[MATERIAL_AMBIENT] = vec4(1.0, 0.0, 1.0, 1.0);
+	result[MATERIAL_DIFFUSE] = vec4(1.0, 0.8, 0.0, 1.0);
+	result[MATERIAL_SPECULAR] = vec4(1.0, 0.8, 0.0, 1.0);
+	result[MATERIAL_SHININESS] = 100.0;
+	
+	result.translation = mat4();				
+	result.rotationX = mat4();
+	result.rotationY = mat4();	
+	result.rotationZ = mat4();	
+	result.scale = mat4();
+	
+	result.setTranslation = function(x, y, z)
+	{
+		result.translation[0][3] = x;
+		result.translation[1][3] = y;
+		result.translation[2][3] = z;
+		
+		result[SHAPE_TRANSLATION_X] = x;
+		result[SHAPE_TRANSLATION_Y] = y;
+		result[SHAPE_TRANSLATION_Z] = z;
+	}
+	
+	result.setRotationX = function(value)
+	{
+		var cosv = Math.cos(value);
+		var sinv = Math.sin(value);
+		
+		result.rotationX[0][0] = 1;
+		result.rotationX[1][1] = cosv;
+		result.rotationX[1][2] = -sinv;
+		result.rotationX[2][1] = sinv;
+		result.rotationX[2][2] = cosv;
+		
+		result[SHAPE_ROTATION_X] = value;
+	}
+
+	result.setRotationY = function(value)
+	{
+		var cosv = Math.cos(value);
+		var sinv = Math.sin(value);
+		
+		result.rotationY[0][0] = cosv;
+		result.rotationY[0][2] = sinv;
+		result.rotationY[1][1] = 1;
+		result.rotationY[2][0] = -sinv;
+		result.rotationY[2][2] = cosv;
+		
+		result[SHAPE_ROTATION_Y] = value;
+	}
+
+	result.setRotationZ = function(value)
+	{
+		var cosv = Math.cos(value);
+		var sinv = Math.sin(value);
+		
+		result.rotationZ[0][0] = cosv;
+		result.rotationZ[0][1] = -sinv;
+		result.rotationZ[1][0] = sinv;
+		result.rotationZ[1][1] = cosv;
+		result.rotationZ[2][2] = 1;
+		
+		result[SHAPE_ROTATION_Z] = value;
+	}
+	
+	result.setScale = function(x, y, z)
+	{
+		result.scale[0][0] = x;
+		result.scale[1][1] = y;
+		result.scale[2][2] = z;
+		
+		result[SHAPE_SCALE_X] = x;
+		result[SHAPE_SCALE_Y] = y;
+		result[SHAPE_SCALE_Z] = z;
+	}
+	
+	result.setColor = function(color)
+	{
+		var colors = result[SHAPE_COLORS];
+		if (!colors)
+		{
+			colors = [];
+			result[SHAPE_COLORS] = colors;
+		}
+		colors.length = 0;
+		var numVerices = result[SHAPE_VERTICES].length / 3;
+		for (var i = 0; i < numVerices; i++)
+		{
+			var numChannels = color.length;
+			for (var j = 0; j < numChannels; j++)
+			{
+				colors[colors.length] = color[j];
+			}
+		}
+	}
+	
+	// NOTE transpose matrices for webgl
+	
+	result.setTranslation(0, 0, -0.5);
+	
+	return result;
+}
+
+function addVertex(vertices, x, y, z)
+{
+	vertices[vertices.length] = x;
+	vertices[vertices.length] = y;
+	vertices[vertices.length] = z;
+}
